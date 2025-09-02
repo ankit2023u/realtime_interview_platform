aaa9269aa8293d2126a5f632048a70a723cb21f0
ankit2023u <ankitguptakusmi@gmail.com>
2025-09-02 17:23:05 +0530

    Auth Basic Frontend

---
+++ components/AuthForm.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import Image from "next/image"
+import Link from "next/link"
+
+const formSchema = z.object({
+    username: z.string().min(2).max(50),
+})
+
+const AuthForm = ({type} : {type: FormType}) => {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+    const isSignIn = type === 'sign-in';
+    return (
+        <div className="card-border lg:min-w-[566px]">
+            <div className="flex flex-col gap-6 card py-14 px-10">
+                <div className="flex flex-row gap-2 justify-center">
+                    <Image src="/logo.svg" alt="logo" height={32} width={38} />
+                    <h2 className="text-primary-100">SmartPrep</h2>
+                </div>
+
+                <h3>Pratice with AI interviewer</h3>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 w-full mt-4 form">
+                        {!isSignIn && <p>Name</p>}
+                        <p>Email</p>
+                        <p>Password</p>
+                        <Button className="btn" type="submit">{isSignIn ? 'Sign in' : 'Register'}</Button>
+                    </form>
+                    <p className="text-center">
+                        {isSignIn ? 'No account yet ?' : 'Have an account already ?'}
+                        <Link href={!isSignIn ? '/sign-in' : '/sign-up'} className="font-bold text-user-primary ml-1">
+                            {!isSignIn ? "Sign in" : "Sign up"}
+                        </Link>
+                    </p>
+                </Form></div>
+        </div>
+    )
+}
+
+export default AuthForm
