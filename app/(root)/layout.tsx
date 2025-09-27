af1a888cafe27644560fece10b8409ffeff8c0e1
ankit2023u <ankitguptakusmi@gmail.com>
2025-09-27 12:43:39 +0530

    Dummy Dashboard UI

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,19 @@
+import Image from 'next/image'
 import { ReactNode } from 'react'
+import Link from 'next/link'
 
 const RootLayout = ({children} : {children: ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="Logo" width={38} height={32} />
+          <h2 className='text-primary-100'>SmartPrep</h2>
+        </Link>
+      </nav>
+
+      {children}
+    </div>
   )
 }
 
